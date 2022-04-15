7a7f597e4f1980d34ef2019740fcb45008f64111
Lydia Reader <lydia.reader@wustl.edu>
2022-04-15 16:53:24 -0500

    Added fields to output

--- model/mechanisms.py
+++ model/mechanisms.py
@@ -8,5 +8,17 @@ def run_lottery_no_gaming(consumers):
     for person in selected:
         person.buy_shoes(cap = 2)
 
-    fields = ['desire', 'identity', 'shoes_acquired']
+    return agents_to_df(consumers)
+
+
+def agents_to_df(consumers):
+    fields = [
+            'abnormal_size',
+            'desire',
+            'num_shoes_want',
+            'money',
+            'identity',
+            'has_loyalty',
+            'shoes_acquired'
+            ]                
     return pd.DataFrame([{field: getattr(person, field) for field in fields} for person in consumers])
