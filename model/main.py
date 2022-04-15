9ed8fe185e2fd4f555bc0b1d309af6879d3e2961
Lydia Reader <lydia.reader@wustl.edu>
2022-04-15 12:52:59 -0500

    Created consumers in main.py

--- model/main.py
+++ model/main.py
@@ -1,7 +1,18 @@
 import consumer
 from shopping_environment import ShoppingEnvironment
 
+# parameters
+num_shoes        = 100
 
+# number of each consumer type
+num_average     = 900
+num_abnormal    = 100
+num_special     = 25
+num_wealthy     = 100
+num_influencers = 50
+num_resellers   = 25
+
+# initialize environment
 env = ShoppingEnvironment(
     mean_desire = 0.5,
     std_desire = 0.2,
@@ -14,5 +25,13 @@ env = ShoppingEnvironment(
     rich_prob_loyal = 0.8,
     reseller_prob_loyal = 0.9
 )
-average_consumer = consumer.AverageConsumer(env)
-average_consumer.printdesire()
+
+# create consumers
+average_consumers    = [consumer.AverageConsumer(env) for _ in range(num_average)]
+abnormal_consumers   = [consumer.AbnormalConsumer(env) for _ in range(num_abnormal)]
+special_consumers    = [consumer.SpecialConsumer(env) for _ in range(num_special)]
+wealthy_consumers    = [consumer.WealthyConsumer(env) for _ in range(num_wealthy)]
+influencer_consumers = [consumer.InfluencerConsumer(env) for _ in range(num_influencers)]
+reseller_consumers   = [consumer.ResellerConsumer(env) for _ in range(num_resellers)]
+
+
