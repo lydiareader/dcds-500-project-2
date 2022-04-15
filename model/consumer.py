f857e938a0bcec91c79f4c3d5180c65ee5080976
Lydia Reader <lydia.reader@wustl.edu>
2022-04-15 12:39:46 -0500

    Added class InfluencerConsumer

--- model/consumer.py
+++ model/consumer.py
@@ -32,9 +32,16 @@ class AbnormalConsumer(Consumer):
 class SpecialConsumer(Consumer):
     def __init__(self):
         self.num_shoes_want = 2
+        self.identity       = "special"
 
 
 class WealthyConsumer(Consumer):
     def __init__(self):
-        self.money = normal(self.env.rich_mean_money, self.env.rich_std_money)
+        self.money       = normal(self.env.rich_mean_money, self.env.rich_std_money)
         self.has_loyalty = random() < self.env.rich_prob_loyal
+        self.identity    = "wealthy"
+
+
+class InfluencerConsumer(WealthyConsumer):
+    def __init__(self):
+        self.identity = "influencer"
